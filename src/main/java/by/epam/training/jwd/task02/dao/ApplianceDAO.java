c746c4d345ea34d88f11e22c3abc37e689bfe0e8
Owl <olga.mailychko@gmail.com>
2020-12-15 23:28:05 +0300

    structure fixed

--- src/main/java/by/epam/training/jwd/task02/dao/ApplianceDAO.java
+++ src/main/java/by/epam/training/jwd/task02/dao/ApplianceDAO.java
@@ -7,6 +7,6 @@ import by.epam.training.jwd.task02.entity.appliance.Appliance;
 import by.epam.training.jwd.task02.entity.criteria.Criteria;
 
 public interface ApplianceDAO {
-	
+
 	<E> List<Appliance> find(Criteria<E> criteria, CriteriaSearchRegexCreator regexCreator);
 }
---
+++ src/test/java/by/epam/training/jwd/task02/SomeTest.java
@@ -0,0 +1,5 @@
+package by.epam.training.jwd.task02;
+
+//notice me github :'c
+public class SomeTest {
+}
